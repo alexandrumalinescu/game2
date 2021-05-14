dcbacd253cabc9ce4da9c9d5d03c944c396e037f
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-05-14 21:32:08 +0300

    keys update

--- script.js
+++ script.js
@@ -17,20 +17,27 @@ let keyCount = 7;
 
 document.addEventListener('keydown', (e)=>{
     let key = e.key;
+    
     if(key==='ArrowDown'){
       keyCount=keyCount+2.7
       document.querySelector('.character-run').style.display='block'
       document.querySelector('.character').style.display='none'
+    
     }else if(key==='ArrowUp'){
       keyCount=keyCount-2.7
-      
-    }else if(key !=='ArrowDown'){
-      document.querySelector('.character').style.display='block'
-      document.querySelector('.character-run').style.display='none'
+      document.querySelector('.character-run').style.display='block'
+      document.querySelector('.character').style.display='none'
     }
     document.querySelector('.character-run').style.left=`${keyCount}vw`
     
 })
+
+setInterval(()=>{
+  document.querySelector('.character').style.display='block'
+  document.querySelector('.character').style.left=`${keyCount}vw`
+  document.querySelector('.character').style.left=`${keyCount-2.7}vw`
+  document.querySelector('.character-run').style.display='none'
+},500)
 
 
 

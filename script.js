898af0aa7c76e16f0aa9cbc2d7086f51605852e1
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-05-16 13:28:30 +0300

    wheel control

--- script.js
+++ script.js
@@ -1,45 +1,67 @@
+let run = document.querySelector(".character-run");
+let stand = document.querySelector(".character");
 
-//let stand1 = document.querySelector('.stand1');
-//let stand2 = document.querySelector('.stand2')
-//let run = document.querySelectorAll('.run')
+let keyCount = 60;
 
-// // setInterval(() => {
-// //     stand1.classList.add('stand2');
-// //     stand2.style.display='block' ;
-// // }, 700);
-///
-// // setInterval(() => {
-// //     stand1.classList.remove('stand2');
-// //     stand2.style.display='none'; 
-// // },1400);
+//  document.addEventListener('keydown', (e)=>{
+//      let key = e.key;
 
-let keyCount = 7;
+//      if(key==='ArrowDown'){
+//        keyCount=keyCount+2.7
+//        run.style.display='block'
+//        stand.style.display='none'
 
-document.addEventListener('keydown', (e)=>{
-    let key = e.key;
-    
-    if(key==='ArrowDown'){
-      keyCount=keyCount+2.7
-      document.querySelector('.character-run').style.display='block'
-      document.querySelector('.character').style.display='none'
-    
-    }else if(key==='ArrowUp'){
-      keyCount=keyCount-2.7
-      document.querySelector('.character-run').style.display='block'
-      document.querySelector('.character').style.display='none'
-    }
-    document.querySelector('.character-run').style.left=`${keyCount}vw`
-    
-})
+//      }else if(key==='ArrowUp'){
+//        keyCount=keyCount-2.7
+//        run.style.display='block'
 
-setInterval(()=>{
-  document.querySelector('.character').style.display='block'
-  document.querySelector('.character').style.left=`${keyCount}vw`
-  document.querySelector('.character').style.left=`${keyCount-2.7}vw`
-  document.querySelector('.character-run').style.display='none'
-},500)
+//        stand.style.display='none'
+//        //run.style.transform='translateX(90)'
+//      }
+//      run.style.left=`${keyCount}vw`
 
+//  })
 
+//
 
+// window.addEventListener("scroll", () => {
 
+//   let sc =scrollY;
+//   console.log(sc)
+//   if(sc===Number){
+//     document.querySelector('.outer-wrapper').style.overflow='initial'
 
+//   }else{
+//     document.querySelector('.outer-wrapper').style.overflow='auto'
+//   }
+
+//   setInterval(()=>{
+//     document.querySelector('.outer-wrapper').style.overflow='initial'
+//   },5)
+
+// })
+
+window.addEventListener("wheel", (event) => {
+  console.log(event.deltaY);
+  let e = event.deltaY;
+  if (e === 125) {
+    keyCount = keyCount + 100;
+    console.log("inainte");
+    run.style.display = "block";
+    stand.style.display = "none";
+  } else {
+    console.log("inapoi");
+    keyCount=keyCount-100
+    run.style.display='block'
+    stand.style.display = "none";
+  }
+  run.style.left = `${keyCount}px`;
+});
+
+ setInterval(() => {
+   stand.style.display = "block";
+   run.style.display = "none";
+   stand.style.left = `${keyCount}px`;
+   
+   
+ }, 500);

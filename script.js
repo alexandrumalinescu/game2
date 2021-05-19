dfeba613770207cf795e570e3487c82201cc3f56
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-05-19 17:30:53 +0300

    animations

--- script.js
+++ script.js
@@ -2,60 +2,35 @@ let run = document.querySelector(".character-run");
 let stand = document.querySelector(".character");
 
 let keyCount = 60;
-
-//  document.addEventListener('keydown', (e)=>{
-//      let key = e.key;
-
-//      if(key==='ArrowDown'){
-//        keyCount=keyCount+2.7
-//        run.style.display='block'
-//        stand.style.display='none'
-
-//      }else if(key==='ArrowUp'){
-//        keyCount=keyCount-2.7
-//        run.style.display='block'
-
-//        stand.style.display='none'
-//        //run.style.transform='translateX(90)'
-//      }
-//      run.style.left=`${keyCount}vw`
-
-//  })
-
-//
-
-// window.addEventListener("scroll", () => {
-
-//   let sc =scrollY;
-//   console.log(sc)
-//   if(sc===Number){
-//     document.querySelector('.outer-wrapper').style.overflow='initial'
-
-//   }else{
-//     document.querySelector('.outer-wrapper').style.overflow='auto'
-//   }
-
-//   setInterval(()=>{
-//     document.querySelector('.outer-wrapper').style.overflow='initial'
-//   },5)
-
-// })
+let mountainCount = 90;
+let mountainCount2 = 300
+let sun=70;
 
 window.addEventListener("wheel", (event) => {
   console.log(event.deltaY);
   let e = event.deltaY;
   if (e === 125) {
+    mountainCount = mountainCount - 1;
+    mountainCount2 = mountainCount2 - 1
+    sun = sun +1
     keyCount = keyCount + 100;
     console.log("inainte");
     run.style.display = "block";
     stand.style.display = "none";
+   
   } else {
     console.log("inapoi");
     keyCount=keyCount-100
+    mountainCount = mountainCount + 1;
+    mountainCount2 = mountainCount2 + 1;
+    sun = sun -1
     run.style.display='block'
     stand.style.display = "none";
   }
   run.style.left = `${keyCount}px`;
+  document.querySelector('.mountain img').style.left=`${mountainCount}vw`;
+  document.querySelector('.mountain2 img').style.left=`${mountainCount2}vw`;
+  document.querySelector('.sun img').style.left=`${sun}vw`
 });
 
  setInterval(() => {

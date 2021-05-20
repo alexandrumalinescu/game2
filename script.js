63ae1203efbffecd1dbf4dbf15e567f481bfc465
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-05-20 16:47:34 +0300

    updates animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let run = document.querySelector(".character-run");
+let run2 = document.querySelector('.character-run2');
 let stand = document.querySelector(".character");
-
 let keyCount = 60;
 let mountainCount = 90;
 let mountainCount2 = 300
@@ -17,24 +17,31 @@ window.addEventListener("wheel", (event) => {
     console.log("inainte");
     run.style.display = "block";
     stand.style.display = "none";
-   
+  
   } else if(e===-125 && keyCount>=160){
     console.log("inapoi");
     keyCount=keyCount-100
     mountainCount = mountainCount + 1;
     mountainCount2 = mountainCount2 + 1;
     sun = sun -1
-    run.style.display='block'
+  
+    run.style.display='none'
+    run2.style.display='block';
+    document.querySelector('.run2').style.display='block';
     stand.style.display = "none";
   }
   run.style.left = `${keyCount}px`;
+  run2.style.left = `${keyCount}px`;
   document.querySelector('.mountain img').style.left=`${mountainCount}vw`;
   document.querySelector('.mountain2 img').style.left=`${mountainCount2}vw`;
   document.querySelector('.sun img').style.left=`${sun}vw`
 });
 
  setInterval(() => {
-   stand.style.display = "block";
-   run.style.display = "none";
-   stand.style.left = `${keyCount}px`;
+   
+     stand.style.display = "block";
+     run.style.display = "none";
+     run2.style.display = "none";
+     stand.style.left = `${keyCount}px`;
  }, 500);
+
